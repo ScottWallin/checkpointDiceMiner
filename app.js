b67171f2775b78e6a3ee11ac801d783c8bbed5c5
Scott Wallin <scott.wallin@me.com>
2023-05-05 15:50:15 -0600

    added functionality. kinda need a nap now

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ let clickUpgrades = [
     name: 'lucky dice',
     price: 100,
     quantity: 0,
-    multiplier: 1
+    multiplier: 1,
+    isBought: false,
   },
   {
     name: 'dice box',
     price: 400,
     quantity: 0,
     multiplier: 10,
+    isBought: false,
   }
 ];
 
@@ -18,41 +20,50 @@ let automaticUpgrades = [
     name: 'nat 20',
     price: 600,
     quantity: 0,
-    multiplier: 20
+    multiplier: 20,
+    isBought: true,
   },
-  // {
-  //   name: 'dice tower',
-  //   price: 2000,
-  //   quantity: 0,
-  //   multiplier: 40
-  // }
+  {
+    name: 'dice tower',
+    price: 2000,
+    quantity: 0,
+    multiplier: 40,
+    isBought: true,
+  }
 ];
 
 
 
 let dice = 0
+// let diceStrength = 0
 
 
 
+// dice = (dice.quantity + (upgradeQuantity * upgradeMultiplier))
 
-
-// SECTION MINING
-function mine() {
+// SECTION MINING & ROLLS(MONEYS)
+function roll() {
+  let foundLuckyUpgrade = clickUpgrades.find(u => u.name == 'lucky dice')
+  let foundBoxUpgrade = clickUpgrades.find(u => u.name == 'dice box')
   // increase dice count by 1
   dice++
+  dice == (dice += (foundBoxUpgrade.multiplier * foundBoxUpgrade.quantity))
+  dice == (dice += (foundLuckyUpgrade.multiplier * foundLuckyUpgrade.quantity))
   // alert(dice)
   console.log('dice')
   drawDice()
 }
 
-// SECTION BUYING UPGRADES
+
+
+// SECTION BUYING CLICK UPGRADES
 function buyLuckyDice() {
   let foundLuckyUpgrade = clickUpgrades.find(u => u.name == 'lucky dice')
   console.log(foundLuckyUpgrade)
   // @ts-ignore
   foundLuckyUpgrade.quantity++
   drawLuckyUpgrade()
-  console.log('purchased')
+  // console.log('purchased')
 }
 function buyDiceBox() {
   let foundBoxUpgrade = clickUpgrades.find(u => u.name == 'dice box')
@@ -61,7 +72,35 @@ function buyDiceBox() {
   drawBoxUpgrade()
 }
 
+// SECTION BUYING PASSIVE UPGRADES
+
+function buyNat20() {
+  let foundNat20Upgrade = automaticUpgrades.find(u => u.name == 'nat 20')
+  foundNat20Upgrade.quantity++
+  // console.log(foundNat20Upgrade)
+  drawNat20Upgrade()
+}
+function buyDiceTower() {
+  let foundDiceTowerUpgrade = automaticUpgrades.find(u => u.name == 'dice tower')
+  foundDiceTowerUpgrade.quantity++
+  // console.log(foundDiceTowerUpgrade)
+  drawDiceTowerUpgrade()
+}
+
+// function collectAutoUpgrades() {
+//   let foundUpgrades = automaticUpgrades.find(u => u.name == 'nat 20')
+
+//   // if (automaticUpgrades.isBought)
+//   // console.log('interval works')
+//   dice += (dice + (automaticUpgrades.quantity *= automaticUpgrades.multiplier))
+//   drawDice()
+
+// }
+
+// SECTION DRAW FUNCTIONS
+
 function drawDice() {
+  // let foundLuckyUpgrade = clickUpgrades.find(u => u.name == 'lucky dice')
   // @ts-ignore
   document.getElementById("dice").innerText = dice
   // diceElem.innerText = dice.toString()
@@ -74,6 +113,21 @@ function drawBoxUpgrade() {
   let foundUpgrades = clickUpgrades.find(u => u.name == 'dice box')
   document.getElementById("dice box").innerText = foundUpgrades.quantity
 }
-drawBoxUpgrade
+function drawNat20Upgrade() {
+  let foundUpgrades = automaticUpgrades.find(u => u.name == 'nat 20')
+  document.getElementById("nat 20").innerText = foundUpgrades.quantity
+}
+function drawDiceTowerUpgrade() {
+  let foundUpgrades = automaticUpgrades.find(u => u.name == 'dice tower')
+  document.getElementById("dice tower").innerText = foundUpgrades.quantity
+}
+
+// SECTION INTERVAL FOR AUTOMATIC UPGRADES
+setInterval(collectAutoUpgrades, 3000)
+
+// SECTION DRAW FUNCTIONS ON OPENING
+drawNat20Upgrade()
+drawDiceTowerUpgrade()
+drawBoxUpgrade()
 drawLuckyUpgrade()
 drawDice()
